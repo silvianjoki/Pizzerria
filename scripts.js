5633b2b5f33d7ffe7f7dd06a16de17ff167e8b75
Silvia Njoki <10silvianjoki@gmail.com>
2022-01-18 11:37:14 +0300

    add javascript commands

--- scripts.js
+++ scripts.js
@@ -138,4 +138,52 @@ $('button.addPizza').click(function(){
 
     var newOrder = new Getpizza(pname,psize,pcrust,ptopping,total);
 
+    $('.ordersmade').append('<tr><td id= "pizzaname">'+newOrder.name + '</td><td id="pizzaSize">' + newOrder.size + '<td><td id= "pizzaCrust>' + newOrder.crust + '</td><td id = "pizzaTopping">' + newOrder.topping + '</td><td id = "total"> '+ mewOrder.total+'</td></tr>');
+    console.log(newOrder);
     });
+
+    // checkout
+$('button#checkout').click(function(){
+    $('button#checkout').hide();
+    $('button.addPizza').hide();
+    $('button.deliver').slideDown(1500);
+    $('#addedprice').slideDown(1500);
+    console.log('Your bill is kes. '+checkoutTotal);
+});
+
+// deliver home
+$('button.deliver').click(function(){
+    $('.table').hide();
+    $('.choice h2').hide();
+    $('.delivery').slideDown(1000);
+    $('#addedprice').hide();
+    $("button.deliver").hide();
+    $('#pizzatotal').hide();
+    let deliveryAmount= checkoutTotal+150;
+    console.log ('The total cost is kes' + deliveryAmount + 'on delivery');
+    $('totalbill').append('Your bill inclusive of delivery is' + deliveryAmount);
+});
+
+// click place order button
+$('button#final-order').click(function(event){
+    event.preventDefault
+});
+
+let person = $('input #name').val();
+let phone = $('input #phone').val();
+let location = $('input#location').val();
+
+if ($('input#name').val() && $('input#phone').val() && $('input#location').val()!=""){
+    $('#finalmessage').append(person+'we have reeived your order and will deliver it at' + location+'the total cost is kes'deliveryAmount);
+    $('#totalbill').hide();
+    $('#finalmessage').slideDown(1000);
+}
+else{
+    alert('Kindly fill in delivery details');
+    $('.delivery').show();
+    $('button#final-order').show();
+}
+preventDefault();
+});
+});
+
