abcfd397939069db215e3530b23567eb2c9bec86
Silvia Njoki <10silvianjoki@gmail.com>
2022-01-18 11:55:25 +0300

    add javascript changes

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var price, crust_price, topping_price ;
 let total=0
-function Getpizza(name,size,crust,topping,total){
+function getPizza(name,size,crust,topping,total){
     this.name = name;
     this.size = size;
     this.crust = crust;
